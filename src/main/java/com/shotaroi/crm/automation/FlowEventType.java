c7dd7942f9c613931664e29715464d600287dd3a
shotaroi <58347058+shotaroi@users.noreply.github.com>
2026-03-03 00:20:05 +0100

    Create FlowEventType.java

---
+++ src/main/java/com/shotaroi/crm/automation/FlowEventType.java
@@ -0,0 +1,8 @@
+package com.shotaroi.crm.automation;
+
+public final class FlowEventType {
+    public static final String LEAD_CREATED = "LEAD.CREATED";
+    public static final String OPPORTUNITY_UPDATED = "OPPORTUNITY.UPDATED";
+
+    private FlowEventType() {}
+}
